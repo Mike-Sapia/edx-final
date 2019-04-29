396260c482130c49fcf88f2901d677568b2850bf
Mike Sapia <Mike.Sapia@plan-b-gmbh.com>
2019-04-29 16:27:06 +0200

    Revert "cmt"
    This reverts commit d907a5f781251fa9178c1067407193b9b015c2ff.

--- edx-final/script.js
+++ edx-final/script.js
@@ -1,6 +1,6 @@
 ﻿//rubric51 creates an alert if button is clicked
 $("#checkout").click(function () {
-    alert("total costs: 1.5$");
+    alert("hi");
 });
 //rubric60 creates an alert if the button is clicked
 $("#send").click(function () {
@@ -11,8 +11,4 @@ $(".b1").click(function () {
 
     $("#test").text("sampeltext");
     $("#test").addClass("testingthis");
-});
-
-$("#tss").click(function () {
-    $("#carousel").attr("data-interval", false);
 });
